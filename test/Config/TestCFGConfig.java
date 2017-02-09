b99d6236383c101d459ddbec95a0d63ca8d7389e
Steffan 'Ikslorin' Soelvsten <201505832@post.au.dk>
2017-02-09 07:49:53 +0100

    More test-cases for the CFG Config
    Based on it already working before, and the test cases cover all the
    different methods, then I don't see any reason for it to add more tests.

--- test/Config/TestCFGConfig.java
+++ test/Config/TestCFGConfig.java
@@ -25,8 +25,25 @@ public class TestCFGConfig {
 
     @Test
     public void ShouldHaveTeamAFiles(){
-        assertThat(
-                config.getString(sc.file_A_name),
-                is(sc.getString(sc.file_A_name)));
+        assertThat(config.getString(sc.file_A_name), is(sc.getString(sc.file_A_name)));
+        assertThat(config.getString(sc.file_A_score), is(sc.getString(sc.file_A_score)));
     }
+
+    //If it already got the third team specific file, then it probably also got the others
+    @Test
+    public void ShouldHaveTeamBFiles(){
+        assertThat(config.getString(sc.file_B_tag), is(sc.getString(sc.file_B_tag)));
+    }
+
+    @Test
+    public void ShouldHaveTrueOnEnablingKeybinds(){
+        assertThat(config.getBoolean(sc.enable_keybindings), is(true));
+    }
+
+    @Test
+    public void ShouldHave72AsSwappingKey(){
+        assertThat(config.getInteger(sc.swap_teams_key), is(72));
+    }
+
+    //TODO: Test the put and putIfAbsent?
 }
